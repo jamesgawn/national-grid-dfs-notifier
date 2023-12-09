ccb8eca297226fd0e67a49789ec74db82847cb6a
James Gawn <james@gawn.uk>
2023-12-09 17:39:20 +0000

    feat: Added initial test request to obtain data from NG api

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"net/http"
 
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
@@ -18,4 +19,12 @@ func main() {
 	}
 
 	log.Info().Msg("Hello World")
+
+	response, err := http.Get("https://api.nationalgrideso.com/api/3/action/datastore_search_sql?sql=SELECT%20*%20FROM%20%20%227914dd99-fe1c-41ba-9989-5784531c58bb%22%20ORDER%20BY%20%22_id%22%20ASC%20LIMIT%20100")
+
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to retrieve DFS data from National Grid API")
+	}
+
+	log.Debug().Int("statusCode", response.StatusCode).Msg("Retrieved DFS data from National Grid API")
 }
